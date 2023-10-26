ec7f7150ed83d778a2392046197bb2589a5ab2ce
Rael Wanjala <raelnwanjala@gmail.com>
2023-10-26 03:10:13 -0700

    Added reaction.js model and installed npm packages

---
+++ models/Reaction.js
@@ -0,0 +1,25 @@
+const mangoose = require('mongoose');
+
+const reactionSchema = new mangoose.Schema({
+    reactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: new mongoose.Types.ObjectId
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true,
+        ref: 'User'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+});
+
+model.exports = reactionSchema;
