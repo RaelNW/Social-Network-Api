3e87a3b90895185f814a14820d5e886500562e05
Rael Wanjala <raelnwanjala@gmail.com>
2023-10-28 04:28:00 -0700

    added data.js and seed.js, added serves.js and updated npm dependencies

--- utils/data.js
+++ utils/data.js
@@ -1,41 +1,114 @@
-const username = [
-    'BlueStar123'
-    'CosmicExplorer'
-    'SilverArrow89'
-    'MidnightWhisper'
-    'ElectricJazz'
-    'RainbowDancer'
-    'PixelPioneer'
-    'SnowflakeNinja'
-    'CrimsonPhoenix'
-    'OceanDreamer'
+const userData = [
+  {
+    username: "BlueStar123",
+    email: "BlueStar123@example.com",
+    firstName: "Blue",
+    lastName: "Star",
+  },
+  {
+    username: "CosmicExplorer",
+    email: "CosmicExplorer@example.com",
+    firstName: "Cosmic",
+    lastName: "Explorer",
+  },
+  {
+    username: "SilverArrow89",
+    email: "SilverArrow89@example.com",
+    firstName: "Silver",
+    lastName: "Arrow",
+  },
+  {
+    username: "MidnightWhisper",
+    email: "MidnightWhisper@example.com",
+    firstName: "Midnight",
+    lastName: "Whisper",
+  },
+  {
+    username: "ElectricJazz",
+    email: "ElectricJazz@example.com",
+    firstName: "Electric",
+    lastName: "Jazz",
+  },
+  {
+    username: "RainbowDancer",
+    email: "RainbowDancer@example.com",
+    firstName: "Rainbow",
+    lastName: "Dancer",
+  },
+  {
+    username: "PixelPioneer",
+    email: "PixelPioneer@example.com",
+    firstName: "Pixel",
+    lastName: "Pioneer",
+  },
+  {
+    username: "SnowflakeNinja",
+    email: "SnowflakeNinja@example.com",
+    firstName: "Snowflake",
+    lastName: "Ninja",
+  },
+  {
+    username: "CrimsonPhoenix",
+    email: "CrimsonPhoenix@example.com",
+    firstName: "Crimson",
+    lastName: "Phoenix",
+  },
+  {
+    username: "OceanDreamer",
+    email: "OceanDreamer@example.com",
+    firstName: "Ocean",
+    lastName: "Dreamer",
+  },
 ];
 
-const email = [
-    'BlueStar123@example.com'
-    'CosmicExplorer@example.com'
-    'SilverArrow89@example.com'
-    'MidnightWhisper@example.com'
-    'ElectricJazz@example.com'
-    'RainbowDancer@example.com'
-    'PixelPioneer@example.com'
-    'SnowflakeNinja@example.com'
-    'CrimsonPhoenix@example.com'
-    'OceanDreamer@example.com'
+const thoughtData = [
+  {
+    username: "BlueStar123",
+    thoughtText:
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+  },
+  {
+    username: "CosmicExplorer",
+    thoughtText:
+      "Life is really simple, but we insist on making it complicated.",
+  },
+  {
+    username: "SilverArrow89",
+    thoughtText: "The only way to do great work is to love what you do.",
+  },
+  {
+    username: "MidnightWhisper",
+    thoughtText: "In the middle of every difficulty lies opportunity.",
+  },
+  {
+    username: "ElectricJazz",
+    thoughtText: "Simplicity is the ultimate sophistication.",
+  },
+  {
+    username: "RainbowDancer",
+    thoughtText:
+      "Your time is limited, don not waste it living a life that is not yours.",
+  },
+  {
+    username: "PixelPioneer",
+    thoughtText:
+      "The biggest risk is not taking any risk. In a world that is changing quickly, the only strategy that is guaranteed to fail is not taking risks.",
+  },
+  {
+    username: "SnowflakeNinja",
+    thoughtText:
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+  },
+  {
+    username: "CrimsonPhoenix",
+    thoughtText:
+      "The best time to plant a tree was 20 years ago. The second best time is now.",
+  },
+  {
+    username: "OceanDreamer",
+    thoughtText:
+      "It does not matter how slowly you go as long as you do not stop.",
+  },
 ];
 
-const thoughts = [
-    'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.'
-    'Life is really simple, but we insist on making it complicated.'
-    'The only way to do great work is to love what you do.'
-    'In the middle of every difficulty lies opportunity.'
-    'Simplicity is the ultimate sophistication.'
-    'Your time is limited, don not waste it living a life that is not yours.'
-    'The biggest risk is not taking any risk. In a world that is changing quickly, the only strategy that is guaranteed to fail is not taking risks.'
-    'The only limit to our realization of tomorrow will be our doubts of today.'
-    'The best time to plant a tree was 20 years ago. The second best time is now.'
-    'It does not matter how slowly you go as long as you do not stop.'
-];
-
-
-module.exports = { username, email, thoughts };
+module.exports = { userData, thoughtData };
--- utils/seed.js
+++ utils/seed.js
@@ -1,4 +1,36 @@
-const db = require('../config/connection');
-const { User, Thought } = require('../models');
+const db = require("../config/connection");
+const { User, Thought } = require("../models");
 
+//import seed data
+const { userData, thoughtData } = require("./data");
+//establish connection
+db.once("open", async () => {
+  console.log("db connected");
+  try {
+    //delete all data
+    await User.deleteMany({});
+    await Thought.deleteMany({});
 
+    //create empty arrays
+    const userData = [];
+    const thoughtData = [];
+
+    //create arrays with data
+    userData.forEach(({ username, email, firstName, lastName }) => {
+      userData.push({ username, email, firstName, lastName });
+    });
+
+    thoughtData.forEach(({ thoughtText, username }) => {
+      thoughtData.push({ thoughtText, username });
+    });
+
+    //seed user and thought data
+    await User.insertMany(userData);
+    await Thought.insertMany(thoughtData);
+    console.log("data seeded");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  } finally {
+    db.close();
+  }
+});
