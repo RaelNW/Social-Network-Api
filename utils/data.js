6daddaede8499cf1169f30af175bf5332cce8e7e
Rael Wanjala <raelnwanjala@gmail.com>
2023-10-28 03:26:45 -0700

    Added seed.js and data.js to utils folder, added email, usernames and thoughts to data.js

---
+++ utils/data.js
@@ -0,0 +1,41 @@
+const username = [
+    'BlueStar123'
+    'CosmicExplorer'
+    'SilverArrow89'
+    'MidnightWhisper'
+    'ElectricJazz'
+    'RainbowDancer'
+    'PixelPioneer'
+    'SnowflakeNinja'
+    'CrimsonPhoenix'
+    'OceanDreamer'
+];
+
+const email = [
+    'BlueStar123@example.com'
+    'CosmicExplorer@example.com'
+    'SilverArrow89@example.com'
+    'MidnightWhisper@example.com'
+    'ElectricJazz@example.com'
+    'RainbowDancer@example.com'
+    'PixelPioneer@example.com'
+    'SnowflakeNinja@example.com'
+    'CrimsonPhoenix@example.com'
+    'OceanDreamer@example.com'
+];
+
+const thoughts = [
+    'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.'
+    'Life is really simple, but we insist on making it complicated.'
+    'The only way to do great work is to love what you do.'
+    'In the middle of every difficulty lies opportunity.'
+    'Simplicity is the ultimate sophistication.'
+    'Your time is limited, don not waste it living a life that is not yours.'
+    'The biggest risk is not taking any risk. In a world that is changing quickly, the only strategy that is guaranteed to fail is not taking risks.'
+    'The only limit to our realization of tomorrow will be our doubts of today.'
+    'The best time to plant a tree was 20 years ago. The second best time is now.'
+    'It does not matter how slowly you go as long as you do not stop.'
+];
+
+
+module.exports = { username, email, thoughts };
---
+++ utils/seed.js
@@ -0,0 +1,4 @@
+const db = require('../config/connection');
+const { User, Thought } = require('../models');
+
+
