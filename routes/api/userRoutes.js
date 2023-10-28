4e380489ccbd8aced50989dd4b50bb03417d3073
Rael Wanjala <raelnwanjala@gmail.com>
2023-10-28 03:12:39 -0700

    Corrected the path on userRoutes and thoughts routes a;so added an index.js to /routes/api

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,10 +1,32 @@
-const express = require('express').Router();
+const router = require('express').Router();
+
 const {
-    getAllThoughts,
-    getThoughtById,
-    createThought,
-    updateThought,
-    deleteThought,
-    addReaction,
-    removeReaction
-} = require('../../controllers/thought-controller');
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/userControllers');
+
+// /api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router
+    .route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend);
+
+module.exports = router;
